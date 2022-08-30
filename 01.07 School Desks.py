24852343593970cc5071d857bb28152a0d2a14c9
Tk2553 <tkdavis@ualr.edu>
2022-08-30 21:33:32 +0000

    a

---
+++ 01.07 School Desks.py
@@ -0,0 +1,5 @@
+a=int(input("Enter Classroom A :"))
+b=int(input("Enter Classroom B :"))
+c=int(input ("Enter Classroom C :"))
+print ( (a//2 + b//2 + c//2 + a%2+b%2+c%2) )
+# Is it possible to add all three together and find the same answer with less characters?
